e31387813fea4bbecca3d82a3d3be3c9a1ba2e07
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-17 10:03:19 -0400

    Solution to exercise 1.3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3");
-const { addUser } = require("./exercises/exercise-1.4");
+//const { addUser } = require("./exercises/exercise-1.4");
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,11 +15,7 @@ express()
   .use(express.urlencoded({ extended: false }))
   .use("/", express.static(__dirname + "/"))
 
-  // exercise 1
-
-  // exercise 2
-
-  // handle 404s
-  .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
+  .get('/exercise-1/users', getUsers)
+  .get('/*', (req, res) => res.status(404).type("txt").send("🤷‍♂️"))
 
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
